e8bd2535c4f549a10f0c101d46794b62c9c0787d
C14\macbru <bruno.machado@sbanken.no>
2024-01-19 14:38:50 +0000

    Final Fix

--- cypress/e2e/step_definitions/completeWorkflow.js
+++ cypress/e2e/step_definitions/completeWorkflow.js
@@ -2,7 +2,7 @@ import {Then} from "cypress-cucumber-preprocessor/steps";
 
   Then("I select the most expensive and add it to the cart", () => {
     cy.get("#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.pricebar > button")
-    .should('be.visible');
+    .should("be.visible");
 
     cy.get("#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.pricebar > button")
     .click();
@@ -10,7 +10,7 @@ import {Then} from "cypress-cucumber-preprocessor/steps";
 
   Then("I select the less expensive and add it to the cart", () => {
     cy.get("#inventory_container > div > div:nth-child(6) > div.inventory_item_description > div.pricebar > button")
-    .should('be.visible');
+    .should("be.visible");
 
     cy.get("#inventory_container > div > div:nth-child(6) > div.inventory_item_description > div.pricebar > button")
     .click();
@@ -18,7 +18,7 @@ import {Then} from "cypress-cucumber-preprocessor/steps";
 
   Then("I remove the most expensive product from the cart", () => {
     cy.get("#inventory_container > div > div:nth-child(6) > div.inventory_item_description > div.pricebar > button")
-    .should('be.visible');
+    .should("be.visible");
 
     cy.get("#inventory_container > div > div:nth-child(6) > div.inventory_item_description > div.pricebar > button")
     .click();
@@ -26,7 +26,7 @@ import {Then} from "cypress-cucumber-preprocessor/steps";
 
   Then("i select a diferent product from the ones before", () => {
     cy.get("#inventory_container > div > div:nth-child(3) > div.inventory_item_description > div.pricebar > button")
-    .should('be.visible');
+    .should("be.visible");
 
     cy.get("#inventory_container > div > div:nth-child(3) > div.inventory_item_description > div.pricebar > button")
     .click();
